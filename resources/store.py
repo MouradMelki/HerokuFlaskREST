0c2bf45d25c9da62e9158edf148239e637ea72a4
Mourad Melki <mourad_melki@hotmail.co.uk>
2021-01-12 16:37:03 +0100

    First commit, added REST API to access items, stores, and user authentication.

---
+++ resources/store.py
@@ -0,0 +1,45 @@
+from flask_restful import Resource, reqparse
+from flask_jwt import jwt_required
+from models.store import StoreModel
+
+class Store(Resource):
+    @jwt_required()
+    def get(self, name):
+        store = StoreModel.find_by_name(name)
+        if store:
+            return store.json()
+        return {'message': 'store not found'}, 404
+    
+    def post(self, name):
+        if StoreModel.find_by_name(name):
+            return {'message': "An store with name '{}' already exists.".format(name)}, 400
+
+        store = StoreModel(name)
+        try:
+            store.save_to_db()
+        except:
+            return {"message": "An error occured inserting the store"}, 500 # Internal server error
+        return store.json(), 201
+    
+    def delete(self, name):
+        store = StoreModel.find_by_name(name)
+        if store:
+            store.delete_from_db()
+
+        return {'message': 'store deleted'}
+
+    def put(self, name):
+        store = StoreModel.find_by_name(name)
+        if store is None:
+            store = StoreModel(name)
+        else:
+            store.price = data['price']
+        store.save_to_db()
+
+        return store.json()
+
+
+class StoreList(Resource):
+    def get(self):
+        return {'stores': [store.json() for store in StoreModel.query.all()]}
+        # return {'stores': list(map(lambda x: x.json(), StoreModel.query.all()))}
