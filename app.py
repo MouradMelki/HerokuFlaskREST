d96586ea678815deb36ec98847d059e2d18951ea
Mourad Melki <mourad_melki@hotmail.co.uk>
2021-01-21 19:00:34 +0100

    jwt extended

--- app.py
+++ app.py
@@ -2,10 +2,14 @@ import os
 
 from flask import Flask
 from flask_restful import Api
-from flask_jwt import JWT
+from flask_jwt_extended import JWTManager
 
-from security import authenticate, identity
-from resources.user import UserRegister
+from resources.user import (
+    UserRegister, 
+    User, 
+    UserLogin, 
+    TokenRefresh
+)
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 from db import db
@@ -15,16 +19,38 @@ from db import db
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db') # it can be any database
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'jose' #Shouldn't be visible by the user
+# app.config['JWT_SECRET_KEY']
 api = Api(app)
 
-jwt = JWT(app, authenticate, identity) # /auth
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+jwt = JWTManager(app) # don't create an /auth
+
+@jwt.user_claims_loader
+def add_claims_to_jwt(identity):
+    if identity == 1: # instead of hard-coding, you should read from a config file or a database
+        return {'is_admin': True}
+    return {'is_admin': False}
+
+@jwt.expired_token_loader
+def expired_token_callbacl():
+    return jsonify({
+        'description': 'The token has expired.',
+        'error': 'token_expired'
+    }), 401
 
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(StoreList, '/stores')
 api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
+api.add_resource(User, '/user/<int:user_id>')
+api.add_resource(UserLogin, '/login')
+api.add_resource(TokenRefresh, '/refresh')
 
 if __name__ == '__main__':
     db.init_app(app)
--- resources/store.py
+++ resources/store.py
@@ -1,9 +1,9 @@
 from flask_restful import Resource, reqparse
-from flask_jwt import jwt_required
+from flask_jwt_extended import jwt_required
 from models.store import StoreModel
 
 class Store(Resource):
-    @jwt_required()
+    @jwt_required
     def get(self, name):
         store = StoreModel.find_by_name(name)
         if store:
@@ -41,5 +41,5 @@ class Store(Resource):
 
 class StoreList(Resource):
     def get(self):
-        return {'stores': [store.json() for store in StoreModel.query.all()]}
+        return {'stores': [store.json() for store in StoreModel.find_all()]}
         # return {'stores': list(map(lambda x: x.json(), StoreModel.query.all()))}
